eb00b0c83179c2c8a2fc7959b79d099e9a541265
thaind97git <thaind97.dev@gmail.com>
2020-09-13 18:37:58 +0700

    change name PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config({
   path: "env/.env",
 });
 
-const port = parseInt(process.env.NODE_PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
