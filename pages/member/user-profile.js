725fb110b10bfe00b27c236069acb8d1c7f51213
rko2709 <duyquanghoang27@gmail.com>
2020-09-27 11:07:29 +0700

    add user profile page

---
+++ pages/member/user-profile.js
@@ -0,0 +1,10 @@
+import UserProfileComponent from "../../component/UserProfileComponent";
+import MemberLayout from "../../layouts/MemberLayout";
+
+export default function UserProfilePage() {
+  return (
+    <MemberLayout>
+      <UserProfileComponent/>
+    </MemberLayout>
+  );
+}
