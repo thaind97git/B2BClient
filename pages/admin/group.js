71f3708233b1700b69e02a4446cf74154a1a82f7
rko2709 <duyquanghoang27@gmail.com>
2020-09-29 06:54:00 +0700

    UserProfileEditComponent
    Add UserProfileEditComponent

---
+++ pages/admin/group.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Messenger from '../../component/Chat/Messenger';
+import MemberLayout from "../../layouts/MemberLayout";
+
+export default function Home() {
+    return (
+        <MemberLayout>
+            <Messenger />
+        </MemberLayout>
+    );
+}
