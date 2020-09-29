cc7f36bfae0815ab23debef0d432740e092f1879
thaind97git <thaind97.dev@gmail.com>
2020-09-29 17:06:02 +0700

    fix import

--- pages/admin/group.js
+++ pages/admin/group.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import Messenger from '../../component/Chat/Messenger';
+import React from "react";
+// import Messenger from '../../component/Chat/Messenger';
 import MemberLayout from "../../layouts/MemberLayout";
 
 export default function Home() {
-    return (
-        <MemberLayout>
-            <Messenger />
-        </MemberLayout>
-    );
+  return <MemberLayout>{/* <Messenger /> */}</MemberLayout>;
 }
