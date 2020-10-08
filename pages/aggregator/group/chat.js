c4700a895a477e0c16d25404fbccf4770210f7b1
rko2709 <duyquanghoang27@gmail.com>
2020-10-08 22:21:06 +0700

    aggregator groupchat
    add groupchat for aggregator and add option in aggregator menu

---
+++ pages/aggregator/group/chat.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AggregatorLayout from "../../../layouts/AggregatorLayout";
+import GroupChatComponent from "../../../component/GroupChatComponent";
+
+export default function Home() {
+  return (
+    <AggregatorLayout>
+      <GroupChatComponent />
+    </AggregatorLayout>
+  );
+}
