8fd47d354cad424aeebbe0e2794c123074cc2775
rko2709 <duyquanghoang27@gmail.com>
2020-10-18 19:36:19 +0700

    add admin productlist and productcreate

---
+++ pages/admin/product/create.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AdminProductCreateComponent from "../../../component/AdminProductCreateComponent";
+import AdminLayout from "../../../layouts/AdminLayout";
+const Page = () => {
+  return (
+    <AdminLayout>
+      <AdminProductCreateComponent />
+    </AdminLayout>
+  );
+};
+export default Page;
