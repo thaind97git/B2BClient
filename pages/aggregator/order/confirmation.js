ff7ad3c7e05e2dbbdee37a28287ecc12346dd443
rko2709 <duyquanghoang27@gmail.com>
2020-10-21 17:50:33 +0700

    aggregator order confirmation

---
+++ pages/aggregator/order/confirmation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ConfirmationOrderComponent from "../../../component/ConfirmationOrderComponent";
+import AggregatorLayout from "../../../layouts/AggregatorLayout";
+const Page = () => {
+  return (
+    <AggregatorLayout>
+      <ConfirmationOrderComponent />
+    </AggregatorLayout>
+  );
+};
+export default Page;
