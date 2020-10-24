0efd6926d4562d6c56b6470e389f873418b4c405
rko2709 <duyquanghoang27@gmail.com>
2020-10-24 07:06:04 +0700

    biddingstatus label and mockdata update

--- component/AdminBiddingManagementComponent.js
+++ component/AdminBiddingManagementComponent.js
@@ -13,9 +13,9 @@ function callback(key) {
 const dataSource = [
   {
     key: "1",
-    name: "IR Night Vision Hidden Camera Watch Sport - 24/10/2020",
+    name: "IR Night Vision Hidden Camera Watch Sport - 25/10/2020",
     duration: "30 Minutes",
-    dateCreated: "January 30, 2020 17:00 GTM",
+    dateCreated: "October 22, 2020 17:00 GTM",
     createdBy: "John Smith",
     status: <BiddingStatusComponent status={B_FEATURE} />,
     actions: (
@@ -24,35 +24,35 @@ const dataSource = [
       </Button>
     ),
   },
-  // {
-  //   key: "2",
-  //   name: "Group 2",
-  //   duration: "1 Hour",
-  //   dateCreated: "January 30, 2020 17:00 GTM",
-  //   createdBy: "Ryota",
-  //   status: <BiddingStatusComponent status={B_CLOSED} />,
-  // },
-  // {
-  //   key: "2",
-  //   name: "Group 3",
-  //   duration: "2 Hours",
-  //   dateCreated: "January 30, 2020 17:00 GTM",
-  //   createdBy: "Ryota",
-  //   status: <BiddingStatusComponent status={B_DONE} />,
-  // },
-  // {
-  //   key: "2",
-  //   name: "Group 4",
-  //   duration: "2 Hours",
-  //   dateCreated: "January 30, 2020 17:00 GTM",
-  //   createdBy: "Ryota",
-  //   status: <BiddingStatusComponent status={B_ACTIVE} />,
-  //   actions: (
-  //     <Button size="small" danger>
-  //       Cancel
-  //     </Button>
-  //   ),
-  // },
+  {
+    key: "2",
+    name: "Followsun 50 in 1 Accessories for Go pro, Action Camera Mounts, Sports Camera Head Strap Chest Strap - 24/10/2020",
+    duration: "2 Hours",
+    dateCreated: "October 18, 2020 17:00 GTM",
+    createdBy: "Ryota",
+    status: <BiddingStatusComponent status={B_ACTIVE} />,
+    actions: (
+      <Button size="small" danger>
+        Cancel
+      </Button>
+    ),
+  },
+  {
+    key: "3",
+    name: "Customization japanese School Uniform shirts wholesale Set - 27/09/1999",
+    duration: "1 Hour",
+    dateCreated: "September 25, 2020 17:00 GTM",
+    createdBy: "Ryota",
+    status: <BiddingStatusComponent status={B_CLOSED} />,
+  },
+  {
+    key: "4",
+    name: "Factory Direct Supply Double-breasted Breathable Chef Cook Uniform - 28/05/2020",
+    duration: "2 Hours",
+    dateCreated: "May 27, 2020 17:00 GTM",
+    createdBy: "Ryota",
+    status: <BiddingStatusComponent status={B_DONE} />,
+  }
 ];
 
 const columns = [
