7744a18991c742754719b6e1528e752a5f18e122
thaind97git <thaind97.dev@gmail.com>
2020-10-27 23:28:22 +0700

    refactor message css

--- component/Chat/Compose/index.js
+++ component/Chat/Compose/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-
-export default function Compose(props) {
+import React, { Fragment, useState } from "react";
+import { doFunctionWithEnter } from "../../../utils";
+export default function Compose({ sendMessage }) {
+  const [message, setMessage] = useState("");
   return (
-    <div>
+    <Fragment>
       <link
         rel="stylesheet"
         type="text/css"
@@ -10,15 +11,21 @@ export default function Compose(props) {
       />
       <div className="compose">
         <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
           type="text"
           className="compose-input"
           placeholder="Type a message, @name"
+          onKeyPress={(event) =>
+            doFunctionWithEnter(event, () => {
+              console.log({ message });
+              !!message && sendMessage(message);
+            })
+          }
         />
 
-        {
-          props.rightItems
-        }
+        {/* {props.rightItems} */}
       </div>
-    </div>
+    </Fragment>
   );
 }
--- component/Chat/Message/index.js
+++ component/Chat/Message/index.js
@@ -1,35 +1,27 @@
-import React from 'react';
-import moment from 'moment';
+import React, { useState } from "react";
+import moment from "moment";
 
 export default function Message(props) {
-  const {
-    data,
-    isMine,
-    startsSequence,
-    endsSequence,
-    showTimestamp
-  } = props;
+  const [message, setMessage] = useState("");
+  const { data, isMine, startsSequence, endsSequence, showTimestamp } = props;
 
-  const friendlyTimestamp = moment(data.timestamp).format('LLLL');
+  const friendlyTimestamp = moment(data.timestamp).format("LLLL");
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <link
         rel="stylesheet"
         type="text/css"
         href="/static/assets/chat/Message.css"
       />
-      <div className={[
-        'message',
-        `${isMine ? 'mine' : ''}`,
-        `${startsSequence ? 'start' : ''}`,
-        `${endsSequence ? 'end' : ''}`
-      ].join(' ')}>
-        {
-          showTimestamp &&
-          <div className="timestamp">
-            {friendlyTimestamp}
-          </div>
-        }
+      <div
+        className={[
+          "message",
+          `${isMine ? "mine" : ""}`,
+          `${startsSequence ? "start" : ""}`,
+          `${endsSequence ? "end" : ""}`,
+        ].join(" ")}
+      >
+        {showTimestamp && <div className="timestamp">{friendlyTimestamp}</div>}
 
         <div className="bubble-container">
           <div className="bubble" title={friendlyTimestamp}>
