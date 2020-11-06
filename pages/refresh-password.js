4e68bab2e1609d0bf921af7aeeef35afbc69fdad
rko2709 <duyquanghoang27@gmail.com>
2020-11-06 12:55:16 +0700

    forgot password and userprofile update

---
+++ pages/refresh-password.js
@@ -0,0 +1,10 @@
+import RefreshPasswordComponent from "../component/RefreshPasswordComponent";
+import UserLayout from "../layouts/UserLayout";
+
+export default function Home() {
+  return (
+    <UserLayout>
+      <RefreshPasswordComponent />
+    </UserLayout>
+  );
+}
