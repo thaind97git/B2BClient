81b71c7b67b7c55420f9cc95350ce690e9b1126b
thaind97git <thaind97.dev@gmail.com>
2020-11-25 20:15:16 +0700

    feedback type color

--- component/Utils/FeedbackTypeComponent.js
+++ component/Utils/FeedbackTypeComponent.js
@@ -1,18 +1,22 @@
 import { Tag } from 'antd';
 import React from 'react';
 import { F_AUCTION, F_RFQ, F_SYSTEM, F_ORDER } from '../../enums/feedbackType';
-
+import {
+  LineChartOutlined,
+  ScheduleOutlined,
+  BugOutlined
+} from '@ant-design/icons';
 const FeedbackTypeComponent = ({ status }) => {
   const getColorByStatus = (status) => {
     switch (status) {
       case F_AUCTION:
-        return '#fcba03';
+        return 'cyan';
       case F_RFQ:
-        return '#03fc28';
+        return 'blue';
       case F_SYSTEM:
-        return '#037bfc';
+        return 'error';
       case F_ORDER:
-        return '#d500ed';
+        return 'blue';
       default:
         break;
     }
@@ -31,7 +35,26 @@ const FeedbackTypeComponent = ({ status }) => {
         break;
     }
   };
-  return <Tag color={getColorByStatus(status)}>{getLabelByStatus(status)}</Tag>;
+  const getIconByStatus = (status) => {
+    switch (status) {
+      case F_AUCTION:
+        return <LineChartOutlined />;
+      case F_RFQ:
+        return <ScheduleOutlined />;
+      case F_SYSTEM:
+        return <BugOutlined />;
+      case F_ORDER:
+        return <ScheduleOutlined />;
+
+      default:
+        break;
+    }
+  };
+  return (
+    <Tag icon={getIconByStatus(status)} color={getColorByStatus(status)}>
+      {getLabelByStatus(status)}
+    </Tag>
+  );
 };
 
 export default FeedbackTypeComponent;
